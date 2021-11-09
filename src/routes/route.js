ec10bda7c985b5777dfa00f6e1a14a5cc81e1ef9
SHUBHAM VERMA <60481686+shubham9471@users.noreply.github.com>
2021-11-09 22:28:09 +0530

    Week4 day 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,4 +6,80 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/movies', function (req, res) {
+    let arr = ['la la land', 'titanic', 'harry potter', 'vampire diaries', 'orginals']
+ 
+    res.send(arr)
+});
+
+router.get('/abc/:index', function (req, res) {
+
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    let i = req.params.index
+
+    if(i>fruits.length-1){
+        res.send("index out of range")
+    }
+    else{
+        res.send(fruits[i])
+    }
+});
+
+
+router.get('/films', function (req, res) {
+    let arr = [ {
+                   "id": 1,
+                  "movies": "The Shining"
+                }, 
+                
+                {
+                    "id": 2,
+                    "movies": "Incendies"
+                }, {
+                    "id": 3,
+                    "movies": "Rang de Basanti"
+                }, {
+                    "id": 4,
+                    "movies": "Finding Demo"
+                }]
+ 
+    res.send(arr)
+});
+
+router.get('/Bollywood/:filmid', function (req, res) {
+
+    let arr = [ {
+        id: 1,
+        movies: "The Shining"
+     }, 
+     
+     {
+         id: 4,
+         movies: "Incendies"
+     }, {
+         id: 3,
+         movies: "Rang de Basanti"
+     }, {
+          id: 6,
+         movies: "Finding Demo"
+     }]
+    
+    let i = req.params.filmid
+    //console.log(typeof i)
+
+    const film = arr.filter(x => x.id == i)
+    console.log(film)
+    if (film.length == 0){
+        res.send("Not found")
+    }
+
+    else{
+        res.send(film)
+    }
+
+
+
+
+});
+
 module.exports = router;
