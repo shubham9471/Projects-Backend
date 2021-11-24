0c7c1653f9ccbac8903c3160824b9773fed5237f
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-11-24 19:09:39 +0530

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -11,11 +11,12 @@ const loginUser = async function (req, res) {
     let givenName = req.body.name;
     let givenPassward = req.body.passward;
         
-    let data = await userModel.find({name: givenName, passward: givenPassward, isDeleted: false})
-
+    let data = await userModel.findOne({name: givenName, passward: givenPassward, isDeleted: false})
+    console.log("id", data._id)
     if(data){
-        let token = jwt.sign({userId: data._id}, "dhiraj")
-
+        let payload = {_id: data._id}
+        let token = jwt.sign(payload, "secret")
+        
         res.header('x-auth-token', token);
         res.send({
             status: true,
@@ -34,37 +35,51 @@ const loginUser = async function (req, res) {
 
 
 const getuser = async function(req, res){
+    if (req.validToken._id == req.params.userId){
+        let users = await userModel.findOne({_id: req.params.userId})
 
+        if (users){
+            res.status(200).send({status: true, data: users})
+        }
     
+        else{
+            res.status(404).send({status: false, data : "Not found"})
+        }
 
-    let users = await userModel.findOne({_id: req.params.userId})
-
-    if (users){
-        res.status(200).send({status: true, data: users})
     }
 
     else{
-        res.status(404).send({status: false, data : "Not found"})
+        res.send({err: "not a valid token"})
     }
+
     
 
 }
 
 
 const update = async function(req, res){
+    if (req.validToken._id == req.params.userId){
 
-    let userbody = await userModel.findOne({_id: req.params.userId})
-    
-    if (userbody){
-        let updatedbody = await userModel.findOneAndUpdate({_id : userbody._id} , {"email" : "shanu@gmail.com"}, {new : true})
+        let userbody = await userModel.findOne({_id: req.params.userId})
 
-        res.send({updatedbody})
+        if (userbody){
+            let updatedbody = await userModel.findOneAndUpdate({_id : userbody._id} , {"email" : "shubham@gmail.com"}, {new : true})
 
+            res.send({updatedbody})
+
+        }
+
+        else{
+            res.send({error : " error"})
+        }
     }
 
     else{
-        res.send({error : " error"})
+        res.send({err: "not a valid token"})
     }
+
+
+
 
 
 }
