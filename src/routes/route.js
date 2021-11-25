bb4900223d57b7de365af7f0a9551e5315f10ae2
SHUBHAM VERMA <60481686+shubham9471@users.noreply.github.com>
2021-11-25 20:08:02 +0530

    Solution Code for Axios Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+const cowinController= require("../controllers/cowinController")
+const tempController= require("../controllers/TempertureController")
+
+router.get("/cowin/states", cowinController.getStatesList)
+router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
+router.get("/cowin/centers", cowinController.getByPin)
+router.post("/cowin/getOtp", cowinController.getOtp)
+
+
+router.get("/london/londontemp", tempController.LondonWeather)
+
+router.get("/london", tempController.LondonCitytemp)
+
+router.get("/sortCityByTemp", tempController.sortCityByTemp)
 
 module.exports = router;
