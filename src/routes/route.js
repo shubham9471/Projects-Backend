08611a0150d1224ffbbf0e76d4c2d2361042deea
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-12-01 11:41:45 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,9 +10,9 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.post('/createAuthor',AuthorController.createAuthor)
-router.post('/createBlogs',BlogsController.createBlogs)
-router.get('/getBlogs',BlogsController.getBlogs)
+router.post('/createAuthor', AuthorController.createAuthor)
+router.post('/createBlogs',Middleware.mid1, BlogsController.createBlogs)
+router.get('/getBlogs',Middleware.mid1, BlogsController.getBlogs)
 router.put('/update/:blogId',Middleware.mid1,BlogsController.update)
 router.delete('/DeleteBlogs/:deleteId',Middleware.mid1,BlogsController.DeleteBlogs)
 router.delete('/DeleteBlogsbyQuery',Middleware.mid1,BlogsController.DeleteBlogsbyQuery)
