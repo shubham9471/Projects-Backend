4e05c265deae5c1110c7448bb169fd8247de20cb
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-12-01 16:10:41 +0530

    Update Middleware.js

--- src/middleware/Middleware.js
+++ src/middleware/Middleware.js
@@ -1,24 +1,31 @@
 const jwt=require("jsonwebtoken")
 
 const mid1= async function(req, res, next)
-{
-    let token=req.headers['x-api-key']
-    
-    if(!token)
-    {
-        return res.send({ status: false, Message: 'No token found' })
-    }else
-    {
-        let decodedtoken = jwt.verify(token, 'Group4')
-        //  console.log(decodedtoken)
-        if(decodedtoken)
+{   try {
+        let token=req.headers['x-api-key']
+        
+        if(!token)
         {
-            req.decodedtoken=decodedtoken
-            next();
-        }else{
-            res.status(404).send({Message:"Not valid Token"})
+            return res.status(404).send({ status: false, Message: 'No token found' })
+        }else
+        {
+            let decodedtoken = jwt.verify(token, 'Group4')
+            //console.log(decodedtoken)
+            if(decodedtoken)
+            {
+                req.decodedtoken=decodedtoken
+                next();
+            }else{
+                res.status(404).send({Message:"Not valid Token"})
+            }
         }
     }
+
+    catch{
+        res.status(400).send({status: false, err: "Something went wrong"})
+    }
+    
+    
 }
 
 module.exports.mid1=mid1
