c34ac5ecf004a9635d925a45a8a5094e8c81d044
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-12-07 15:20:32 +0530

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -17,74 +17,66 @@ const createColleges = async function (req, res) {
     try {
 
         const requestBody = req.body;
+        if(!isValidRequestBody(requestBody)) {
+            res.status(400).send({status: false, message: 'Invalid request parameters. Please provide college details'})
+            return
+        }
+        
         let temp = requestBody.name
          
-            if(!isValid(temp)) {
-                res.status(400).send({status: false, message: 'Name is required'})
-                return
-            }
+        if(!isValid(temp)) {
+            res.status(400).send({status: false, message: 'Name is required'})
+            return
+        }
     
-            else{
-                // in case there is a spacing in collge abbreviation 
+        
+        // in case there is a spacing in collge abbreviation 
+
+        let properspacing = temp.replace(/\s+/g, '') // this will remove all spacing by recursively/ constinously calling regex untill there is no space in b/w
+        if (!isAlpha(properspacing)){
+            res.status(400).send({err: "Invalid request parameters-> Please provide valid college abbreviation. The abbreviation name should only contains alphabets. "})
+            return
+        }
+
+        
+        //in case the name is not in lower case 
+        let Name = properspacing.toLowerCase()
     
-                let properspacing = temp.replace(/\s+/g, '') // this will remove all spacing by recursively/ constinously calling regex untill there is no space in b/w
-                if (!isAlpha(properspacing)){
-                    res.status(400).send({err: "Invalid request parameters-> Please provide valid college abbreviation. The abbreviation name should only contains alphabets. "})
-                }
+        requestBody.name = Name
 
-                else{
-                    //in case the name is not in lower case 
-                    Name = properspacing.toLowerCase()
-        
-                    requestBody.name = Name
-                    if(!isValidRequestBody(requestBody)) {
-                        res.status(400).send({status: false, message: 'Invalid request parameters. Please provide college details'})
-                        return
-                    }
-        
-                    // Extract params
-                    const {name, fullName, logo } = requestBody;
-        
-        
-                    // Validation starts
-                    // WE ALREADY VALIDATED THE PRESENCE OF NAME ABOVE IN LINE 22
-                    // if(!isValid(name)) {
-                    //     res.status(400).send({status: false, message: 'Name is required'})
-                    //     return
-                    // }
 
-                    
-                    if(!isValid(logo)) {
-                        res.status(400).send({status: false, message: 'logo is required'})
-                        return
-                    }
-                    
-                    if(!isValid(fullName)) {
-                        res.status(400).send({status: false, message: 'fullName is required'})
-                        return
-                    }
-                    // cleaning up the full name 
-                    else {
-                        let tempN = req.body.fullName
-                        
-                        let tempname = tempN.replace(/_/g, ' ') // replacing all underscore with space
-                
-                        req.body.fullName = tempname
-                    }
-                    
-                    const collegeData = {
-                        name ,
-                        fullName ,
-                        logo
-                    }
-                    
+        // Extract params
+        const {name, fullName, logo } = requestBody;
+
+
+        if(!isValid(logo)) {
+            res.status(400).send({status: false, message: 'logo is required'})
+            return
+        }
         
-                    let data = await collegeModel.create(collegeData);
-                    res.status(201).send({ status: true, data: data })
+        if(!isValid(fullName)) {
+            res.status(400).send({status: false, message: 'fullName is required'})
+            return
+        }
 
-                }
+        // cleaning up the full name 
+    
+        let tempN = req.body.fullName
+        
+        let tempname = tempN.replace(/_/g, ' ') // replacing all underscore with space
 
-            }
+
+        const collegeData = {
+            name : name ,
+            fullName :  tempname,
+            logo: logo
+        }
+        
+
+        let data = await collegeModel.create(collegeData);
+        res.status(201).send({ status: true, data: data })
+
+           
 
     }
     catch (err) {
